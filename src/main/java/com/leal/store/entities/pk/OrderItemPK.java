a3f74c3d79fda65b47184a402cd62c48c2ca34cb
Marivaldo-Leal <marivaldo-@hotmail.com>
2025-06-22 14:24:04 -0300

    feat(OrderItem) : add OrderItem and OrderItemPK

---
+++ src/main/java/com/leal/store/entities/pk/OrderItemPK.java
@@ -0,0 +1,37 @@
+package com.leal.store.entities.pk;
+
+import com.leal.store.entities.Order;
+import com.leal.store.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
